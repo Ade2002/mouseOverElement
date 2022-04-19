561d06751638bb1bf044253c8e6a5abd57af5f12
Ade2002 <sikirusulaimon2002@gmail.com>
2022-04-19 09:27:05 +0100

    started getting the canvas context as 2d

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // setting variables to elements, got the canvas element by default ID variable
 // set c as the canvas context 2d
+// tx asper the x axis detetmining the window inner width
+// ty asper the y axis detetmining the window inner height
 var c = canvas.getContext("2d");
 var tx = window.innerWidth;
 var ty = window.innerHeight;
